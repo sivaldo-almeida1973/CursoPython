c5290971a4ebe2bf7e4db9ba6ccefd2fdbc025f2
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-01-27 20:42:05 -0300

    tipos_variaveis.py /ex_op_mateamticas.py

---
+++ exercicios/ex_op_matematica.py
@@ -0,0 +1,24 @@
+"""
+1) Realizar a media da nota de quatros alunos
+"""
+
+# aluno1 = int(input('Digite a nota do aluno 1:'))
+# aluno2 = int(input('Digite a nota do aluno 2:'))
+# aluno3 = int(input('Digite a nota do aluno 3:'))
+# aluno4 = int(input('Digite a nota do aluno 4:'))
+#
+# media = (aluno1 + aluno2 + aluno3 + aluno4)/ 4
+#
+# print(f'A media das notas são:{media}')
+
+"""
+coverter uma temperatura de graus ºC para ºF e para K
+"""
+
+tempC = float(input('Temperatura em ºC: '))
+tempF = tempC * 1.8 + 32
+print(f'A temperatura em Graus ºC {tempC} convertida para ºF é: {tempF} ')
+
+tempC = float(input('Temperatura em ºC: '))
+tempK = tempC + 273.15
+print(f'A temperatura em Graus ºC {tempC} convertida para ºK é: {tempK} ')
