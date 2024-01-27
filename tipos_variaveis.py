f5c7d3aac66ff9aacbc9d3bde36d516890c21e41
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-01-27 20:22:22 -0300

    tipos_variaveis

---
+++ tipos_variaveis.py
@@ -0,0 +1,22 @@
+"""
+1)variavel global:
+2)variavel local:
+
+como decalrar:
+nome = dado
+idade = 34
+IdadeJoao = 17
+idade_joao = 17
+
+Tipos int:
+num = 10
+Numeros sem casas decimais
+tipos float:
+num = 10.67
+
+
+#------------------------
+num_complexo = 1j
+print(type(num_complexo))
+
+"""
