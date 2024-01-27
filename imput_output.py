ef5b1d2ddf56972877278ab712690b33fb9a9fcb
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-01-27 19:44:12 -0300

    criada a pagina impu_output.py

---
+++ imput_output.py
@@ -0,0 +1,41 @@
+"""
+imput => recebe dados do usuário
+dados recebidos do tipo strings
+
+output - print => apresenta dados ao usuário
+
+"""
+# cor = 'vemelho'
+
+# cor = input('Digite uma cor: ')
+#
+# print('voce escolheu a cor %s' % cor)
+#
+# print('voce escolheu a cor {0}'.format(cor))
+#
+# print(f'Voce escolheu a cor {cor}')
+#
+# print('tomate')
+# print('tomate')
+# print('tomate')
+#
+# #imprime um ao lado do outro
+# print('tomate', end=' ')
+# print('tomate', end=' ')
+# print('tomate', end=' ')
+
+
+
+# cor = input('Digite uma cor: ')
+#
+# numero = input('Digite um numero: ')
+#
+# print(f'Voce digitou a cor {cor} e o  numero {numero}')
+
+#realizar op.
+
+numero1 = input('Digite o primeiro numero: ')
+numero2 = input('Digite o segundo numero: ')
+print(f'a soma é: {int(numero2) + int(numero1)}')
+soma = int(numero2) + int(numero1)
+print(f'A soma entre os numeros {numero1} + {numero2} = soma {soma}')
