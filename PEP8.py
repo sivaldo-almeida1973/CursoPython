bdeac26e0a06382989ea7676bd235b2bd3535e86
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-01-27 19:24:32 -0300

    sobre PEP8

---
+++ PEP8.py
@@ -0,0 +1,43 @@
+"""
+PEP8 => Index of Python enhancement Proposals(indices de prpostas de
+ aprimoramento Python)
+
+//reconhecer palavras em portugues
+file-settings-inspections-proofreadings- daesmarque -typo
+
+identacao 4 espaços(organizar codigo
+
+if x < 10:
+    if y > 20:
+        y = 5
+
+obs: sempre utitlize 4 espaços
+3) linhas em branco:
+sempre duas linhas em branco entre classe e funcoes
+
+def x():
+   pass
+
+
+def y():
+   pass
+
+4)Importacao:
+sempre importe uma biblioteca por vez
+
+import x
+import y
+
+5)nunca use espacos antes de chaves, colchetes e parenteses
+def x():
+    pass
+
+y = 10
+
+6) variaveis=>
+utilize letas minusculas separadas por underline
+utilize letas maiusculas separadas por underline
+utilize palavras separadas por letras maiusculas
+
+
+"""
